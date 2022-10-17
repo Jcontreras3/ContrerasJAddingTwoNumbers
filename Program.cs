2fe6753533ce7b8219715d8a58b5084cbcc319a4
Jovann Contreras <jcontreras@codestack.co>
2022-10-17 15:10:38 -0700

    added datavalidation for num 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Jovann Contreras
+//Date 10/17/2022
+//Getting to integers and adding them together with data validation.
+
+Console.WriteLine("Welcome to Adding Two Numbers Together!!");
+int num1;
+bool n1 = false;
+while (!n1)
+{
+    Console.WriteLine("Enter your first number"); 
+    string NumberOne = Console.ReadLine();
+    n1 = int.TryParse(NumberOne, out num1);
+   
+    if (n1)
+    {
+        Console.Write($"Your first number: {num1}");
+    }
+    else if(!n1)
+    {
+        Console.WriteLine("Not a number");
+    }
+
+}
