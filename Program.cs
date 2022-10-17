5fe264de29c7eb13d25d9a30becb2ea86135368c
Jovann Contreras <jcontreras@codestack.co>
2022-10-17 16:03:43 -0700

    Added the while loop and got the numbers to add

--- Program.cs
+++ Program.cs
@@ -3,45 +3,68 @@
 //Getting to integers and adding them together with data validation.
 
 
-int result;
-int num1 = 0;
-int num2 = 0;
-Console.WriteLine("Welcome to Adding Two Numbers Together!!");
-bool n1 = false;
-while (!n1)
+Console.Clear();
+string playAgain = "yes";
+while (playAgain == "yes")
 {
-    Console.WriteLine("Enter your first number");
-    string NumberOne = Console.ReadLine();
-    n1 = int.TryParse(NumberOne, out num1);
 
-    if (n1)
+
+    int result;
+    int num1 = 0;
+    int num2 = 0;
+    Console.WriteLine("Welcome to Adding Two Numbers Together!!");
+    bool n1 = false;
+    while (!n1)
     {
-        Console.WriteLine($"Your first number: {num1}");
-    }
-    else if (!n1)
-    {
-        Console.WriteLine("Not a number");
+        Console.WriteLine("Enter your first number");
+        string NumberOne = Console.ReadLine();
+        n1 = int.TryParse(NumberOne, out num1);
+
+        if (n1)
+        {
+            Console.WriteLine($"Your first number: {num1}");
+        }
+        else if (!n1)
+        {
+            Console.WriteLine("Not a number");
+        }
+
     }
 
+
+    bool n2 = false;
+    while (!n2)
+    {
+        Console.WriteLine("Enter your second number");
+        string NumberTwo = Console.ReadLine();
+        n2 = int.TryParse(NumberTwo, out num2);
+
+        if (n2)
+        {
+            Console.WriteLine($"Your second number: {num2}");
+        }
+        else if (!n2)
+        {
+            Console.WriteLine("Not a number");
+        }
+    }
+
+    result = num1 + num2;
+    Console.WriteLine($"Answer: {result}");
+
+    Console.WriteLine("Would you like to try again? type in Yes to retry or No to quit");
+    playAgain = Console.ReadLine().ToLower();
+    if (playAgain == "yes")
+    {
+        continue;
+    }
+    else if (playAgain == "no")
+    {
+        Console.WriteLine("Thanks for playing!");
+    }
+    else if (playAgain != "yes" || playAgain != "no")
+    {
+        Console.WriteLine("Not a valid pick please enter in Yes or No");
+        playAgain = Console.ReadLine().ToLower();
+    }
 }
-
-
-bool n2 = false;
-while (!n2)
-{
-    Console.WriteLine("Enter your second number");
-    string NumberTwo = Console.ReadLine();
-    n2 = int.TryParse(NumberTwo, out num2);
-
-    if (n2)
-    {
-        Console.WriteLine($"Your second number: {num2}");
-    }
-    else if (!n2)
-    {
-        Console.WriteLine("Not a number");
-    }
-}
-
-result = num1 + num2;
-Console.WriteLine($"Answer: {result}");
