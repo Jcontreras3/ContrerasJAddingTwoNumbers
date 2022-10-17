fa0c00930138f86ea4ba2f11761922393a334d13
Jovann Contreras <jcontreras@codestack.co>
2022-10-17 15:14:24 -0700

    added validation for num2

--- Program.cs
+++ Program.cs
@@ -7,15 +7,34 @@ int num1;
 bool n1 = false;
 while (!n1)
 {
-    Console.WriteLine("Enter your first number"); 
+    Console.WriteLine("Enter your first number");
     string NumberOne = Console.ReadLine();
     n1 = int.TryParse(NumberOne, out num1);
-   
+
     if (n1)
     {
-        Console.Write($"Your first number: {num1}");
+        Console.WriteLine($"Your first number: {num1}");
     }
-    else if(!n1)
+    else if (!n1)
+    {
+        Console.WriteLine("Not a number");
+    }
+
+}
+
+int num2;
+bool n2 = false;
+while (!n2)
+{
+    Console.WriteLine("Enter your second number");
+    string NumberTwo = Console.ReadLine();
+    n2 = int.TryParse(NumberTwo, out num2);
+
+    if (n2)
+    {
+        Console.WriteLine($"Your second number: {num2}");
+    }
+    else if (!n2)
     {
         Console.WriteLine("Not a number");
     }
